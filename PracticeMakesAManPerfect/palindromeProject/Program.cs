ca2d08fb1cf5dfa6f48b26450edb9fd30913601f
tilak-giri <tilakgiri01@gmail.com>
2024-04-11 11:41:37 +0545

    last push

---
+++ PracticeMakesAManPerfect/palindromeProject/Program.cs
@@ -0,0 +1,24 @@
+﻿// Write a program that converts a string to an integer and adds 10 to it.
+// string s= "100";
+// int i = Convert.ToInt32(s);
+// Console.WriteLine(i + 10);
+
+
+// Write a program that converts a double to an integer and prints the result.
+// double d = 5.5;
+// int i = Convert.ToInt32(d);
+// Console.WriteLine(i);
+
+
+// Write a program that converts a string representing a floating-point number to a double and prints the result.
+// string floatData = "5.5";
+// double doubleData = Convert.ToDouble(floatData);
+// Console.WriteLine(doubleData);
+
+
+// Write a program that converts a character to its ASCII code and prints it.
+
+char character= 't';
+
+int intergerData = (int)character;
+Console.WriteLine(intergerData);
